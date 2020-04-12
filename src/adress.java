c09af3e87008c5bbc15c14cff457517e1c042c8b
Сергій Ковбасюк <63245553+SerhiiKovbasiuk@users.noreply.github.com>
2020-04-12 10:11:38 +0300

    second

---
+++ src/adress.java
@@ -0,0 +1,16 @@
+
+public class adress {
+	
+	public static void main(String[] args) {
+		System.out.println("europe");
+		System.out.println("corp");
+		System.out.println("#    #");
+		System.out.println("#    #");
+		System.out.println("######");
+		System.out.println("#    #");
+		System.out.println("#    #");
+		
+		// TODO Auto-generated method stub
+	}
+	
+}
